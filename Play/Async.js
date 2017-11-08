818b46db0ae83bd0571ca2f04fd5d48817073b02
NexFlare <harshmagarwal@outlook.com>
2017-11-08 12:18:36 +0530

    Input address from user

---
+++ Play/Async.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+
+setTimeout(()=>{
+    console.log("In Time Out ")
+},2000)
+
+setTimeout(()=>{
+    console.log("In Time Out Zone 2")
+},0)
+
+
+console.log("Bye bye")
--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const request=require("request")
+const request = require("request")
+const yargs = require("yargs")
+const argv = yargs.options({
+    a: {
+        demand: true,
+        alias: "address",
+        describe: "Address to fetch weather",
+        string: true
+    }
+}).help().alias("help", "h").argv
+
+let address=encodeURIComponent(argv.a)
 
 request({
-    url: "http://maps.googleapis.com/maps/api/geocode/json?address=Sector%2062",
-    json: true
-},(error,response,body)=>{
-    if(!error)
-        console.log(body)
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+    json: true //Will convert the json data to javascript object
+}, (error, response, body) => {
+    if (!error) {
+        //console.log(JSON.stringify(body, undefined, 2))
+        console.log(body.results[0].geometry.location)
+    }
 })
